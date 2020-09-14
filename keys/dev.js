76c3791d3d6b1efe5fc71454f3ea8ce13f35fd06
Eyal Harush <theharush@gmail.com>
2020-09-14 19:19:52 +0300

    Delete dev.js

--- keys/dev.js
+++
@@ -1,6 +0,0 @@
-// keys to use in dev mode only!
-// DONT commit this
-module.exports = {
-  mongoURI: "mongodb://nadlaneilat:nadlan1234@ds033123.mongolab.com:33123/nadlaneilat",
-  cookieSecret: "cookiesIsMyFav0riteDessert"
-};
